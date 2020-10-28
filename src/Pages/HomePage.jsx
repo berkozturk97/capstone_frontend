9e3f18281119daf4f01044f82f501bb8440ed81e
Berk Ozturk <34316304+berkozturk97@users.noreply.github.com>
2020-10-28 19:54:15 +0300

    login page first commit

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
+import Particles from '../video/videoplayback.mp4';
 
 
 const HomePage = () => {
-    return (
-       <h1>Home Page</h1>
-    );
-}
+  return (
+    <h1>
+      <video autoPlay loop muted>
+          <source src={Particles} type='video/mp4' />
+      </video>
+    </h1>
+  );
+};
 
 export default HomePage;
